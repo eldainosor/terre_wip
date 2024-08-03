0a556c5d85110a6e0de3b006a5b171210b531407
El Dainosor <eldainosor@gmail.com>
2024-08-03 16:45:08 -0300

    vamos a comprar los valores exactos de los tipos de notes disponibles

--- ter_conv.py
+++ ter_conv.py
@@ -146,6 +146,7 @@ def analize_charts(charts:dict, bpm_data:dict, debug = False):
     mods_list = []
     for this_note in charts:
         #TODO: note modes is:   0x00 NOTE "N", 0x01 "S LEN" STAR, 0x10 HOPO "N 5", 0x20 UP,  0x30 DOWN, 0x02 ???
+        #                                                         0x11 HOPO+STAR,  0x21 UP+STAR, 0x31 DOWN+STAR
         note_in = {
             "time":     int(this_note['time']),
             "type":     "N " + str(this_note['note']),
@@ -153,15 +154,15 @@ def analize_charts(charts:dict, bpm_data:dict, debug = False):
         }
         notes_list.append(note_in)
         
-        has_sp = this_note['mods'] & 0x01
-        has_hopo = this_note['mods'] & 0x10
-        has_upstrum = this_note['mods'] & 0x20
+        has_sp = this_note['mods'] & 0x01 == 0x01
+        has_hopo = this_note['mods'] & 0x10 == 0x10
+        has_upstrum = this_note['mods'] & 0x20 == 0x20
         has_other = this_note['mods'] & 0x0E  #DEBUG
         # Unneeded note mode parsing
-        has_sp_and_hopo = this_note['mods'] & 0x11
-        has_sp_and_upstrum = this_note['mods'] & 0x21
-        has_sp_and_downstrum = this_note['mods'] & 0x21
-        has_downstrum = this_note['mods'] & 0x30
+        has_sp_and_hopo = this_note['mods'] & 0x11 == 0x11
+        has_sp_and_upstrum = this_note['mods'] & 0x21 == 0x21
+        has_sp_and_downstrum = this_note['mods'] & 0x21 == 0x21
+        has_downstrum = this_note['mods'] & 0x30 == 0x30
 
         if has_sp:
             note_in = {
